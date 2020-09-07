69aebdffcd85e46354d2e55f96057d7afb02be89
levy <zlw5307@163.com>
2020-09-07 18:04:30 +0800

    fix

--- src/runtime/test/sasl_wrapper_test.cpp
+++ src/runtime/test/sasl_wrapper_test.cpp
@@ -1,6 +1,28 @@
-//
-// Created by mi on 2020/9/7.
-//
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Microsoft Corporation
+ *
+ * -=- Robust Distributed System Nucleus (rDSN) -=-
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 #include "runtime/security/sasl_wrapper.h"
 
@@ -9,19 +31,15 @@
 
 namespace dsn {
 namespace security {
-class sasl_wrapper_test : public testing::Test {
-
-};
-
-TEST_F(sasl_wrapper_test, sasl_wrapper_test) {
+TEST(sasl_wrapper_test, support_gssapi)
+{
     std::unique_ptr<sasl_wrapper> wrapper = create_sasl_wrapper(true);
     error_s err_s = wrapper->init();
-    dassert(err_s.is_ok(), "error is ok");
+    ASSERT_TRUE(err_s.is_ok());
 
     std::string output;
     err_s = wrapper->start("GSSAPI", "", output);
-    ddebug_f("output = {}", output);
-    dassert(err_s.is_ok() || ERR_SASL_INCOMPLETE == err_s.code(), "error is ok or incomplete");
+    ASSERT_TRUE(err_s.is_ok() || ERR_SASL_INCOMPLETE == err_s.code());
 }
-}
-}
+} // namespace security
+} // namespace dsn
